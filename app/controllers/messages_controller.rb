a75603986636fb92b34a66dce7102879e26d277b
y-u-y-a <yuya@yys-MacBook-Air.local>
2019-04-10 16:13:30 +0900

    グループ編集機能の完成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,13 @@
 class MessagesController < ApplicationController
+  before_action :set_group, only: [:index]
 
   # メインページへ
   def index
+  end
+
+  private
+  def set_group
+    @group = Group.find(params[:group_id])
+    # messages#indexを動かす時のparamsを確認すると、group_idにグループのgroup.idが入っていることがわかる
   end
 end

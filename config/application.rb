b91426bdc12b24d3e264cbaddf75c96b0ce25b10
y-u-y-a <yuya@yys-MacBook-Air.local>
2019-04-10 13:47:22 +0900

    フラッシュメッセージ、日本語化

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,6 @@ module ChatStudy
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.default_locale = :ja
   end
 end

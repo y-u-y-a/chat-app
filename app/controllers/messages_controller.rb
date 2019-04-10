de4750918114df7211a784c03596f370951f1353
y-u-y-a <yuya@yys-MacBook-Air.local>
2019-04-10 18:36:12 +0900

    メッセージ送信機能

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,34 @@
 class MessagesController < ApplicationController
-  before_action :set_group, only: [:index]
+  before_action :set_group
 
   # メインページへ
   def index
+    # form_forに使うため
+    @message = Message.new
+    # グループで投稿されたメッセージ全てが格納されている
+    @messages = @group.messages.includes(:user)
+  end
+
+  def create
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: "メッセージが送信されました"
+    else
+      # 保存に失敗した場合、indexアクションと同じように。
+      @messages = @group.message.includes(:user)
+      # flashメッセージを表示させる
+      flash[:alert] = "メッセージを入力してください"
+      # indexビューを表示させる
+      render :index
+    end
   end
 
   private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    # mergeメソッドはハッシュを結合する、paramsを許可するハッシュとuser_idが格納されたハッシュを結合！
+  end
 
   def set_group
     @group = Group.find(params[:group_id])
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,8 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
-  validates :content, presense: true, unless: image?
+  validates :content, presence: true, unless: :image?
+
+  mount_uploader :image, ImageUploader
+
 end
